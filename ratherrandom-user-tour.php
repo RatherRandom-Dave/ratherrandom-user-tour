818d9a747786d885f7e9c0c6afd502cb90dc22ab
Dave Tan <davetan@ratherrandom.website>
2019-12-23 18:55:32 +0800

    Testing out plugin for use in WordPress.

---
+++ ratherrandom-user-tour.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: ratheRRandom User Tour Plugin
+ * Plugin URI: https://www.ratherrandom.website/
+ * Description: This plugin is to allow new users have a quick tour of WordPress.
+ * Version: 1.0
+ * Author: ratheRRandom
+ * Author URI: https://www.ratherrandom.website/
+ */
+
+add_action( 'the_content', 'my_thank_you_text' );
+
+function my_thank_you_text ( $content ) {
+    return $content .= '<p>Thank you for reading!</p>';
+}
